5d58ea87bcc32c5c216c18aee805e20d2f177697
leleco372 <leandroalcantara123456@gmail.com>
2026-07-22 18:33:55 -0300

    ajustes de bugs
    não estava aparecendo mensagem de erro de login e não tava levando a pág agenda.

--- Agenda_medica/main.py
+++ Agenda_medica/main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 from views import *
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True) 
--- Agenda_medica/models.py
+++ Agenda_medica/models.py
@@ -3,17 +3,5 @@ conexao = sqlite3.connect('banco.db')
 cursor = conexao.cursor()
 
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL
-)""")
 
-cursor.execute("SELECT 1 FROM usuarios WHERE email = ?", ('leandro@example.com',))
-if cursor.fetchone() is None:
-    cursor.execute(
-        "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)",
-        ('Leandro', 'leandro@example.com', '123456')
-    )
 conexao.commit()
